aab81181e709a1ff51f2fc943cc7a9c004a8a909
AlexF99 <alexandreopf@gmail.com>
2022-04-07 22:04:58 -0300

    added input parsing

--- main.c
+++ main.c
@@ -2,7 +2,38 @@
 #include <stdlib.h>
 #include "hashtable.h"
 
+#define LENGTH 1024
+
 int main()
 {
+    char op[1];
+    char line[LENGTH];
+    int chave;
+
+    scanf("%s", op);
+    if (op[0] == 'q')
+        exit(0);
+
+    scanf("%d", &chave);
+
+    while (fgets(line, 1024, stdin) != NULL)
+    {
+        scanf("%s", op);
+        switch (op[0])
+        {
+        case 'q':
+            exit(0);
+        case 'i':
+            scanf("%d", &chave);
+            insert(chave);
+            break;
+        case 'r':
+            scanf("%d", &chave);
+            // exclui nodo
+            break;
+        default:
+            break;
+        }
+    }
     return 0;
 }
